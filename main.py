d8cdce04bc5f925cd1ab47a330a15278926384d7
Lily Hritz <lhritz24@biomedscienceacademy.org>
2020-11-10 19:15:25 +0000

    I added the \n command

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
+Author: Lily Hritz
+Period/Core: 7
 
 """
+name1 = input("enter your name with an apostrophe: ")
+adjective1 = input("enter an adjective: ")
+name = input("enter your name: ")
+noun1 = input("enter a noun: ")
+noun2 = input("enter another noun with an apostrophe: ")
+verb1 = input("enter a verb: ")
+verb2 = input("enter another verb: ")
+noun3 = input("enter a noun: ")
+adjective2 = input("enter an adjective: ")
+noun4 = input("enter a noun: ")
+adjective3 = input("enter an adjective: ")
+print("\n")
+print("A Relatable Bio-Med Story")
+print("It was " + name1 + " first day at his/her " + adjective1 + " school. \n he/she was told that the school was under construction. he/she looks at the time and sees class is about to \n start! " + name + " sees a " + noun1 + ". maybe they can help, he/she thought. 'excuse me?' he/she " + verb2 + ", 'can you help me \n find' " + noun2 + " classroom?' 'of course!' " + noun1 + " \n replied and " + verb1 + " how to \n get to the class that he/she was looking for. " + name + " follows what the " + noun1 + " said. he/she " + verb2 + " to class. " + noun3 + " \n asks, 'are you our '" + adjective2 + " student?' 'yes I am.' " + name + " \n replies. " + noun3 + " asks again, 'did you find everything okay?' " + name + " nods. 'this school is pretty " + adjective3 + " I was a bit lost.' 'that's alright, a lot of " + noun4 + " were lost too!' " + noun3 + " reassures.")
